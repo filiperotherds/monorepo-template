033170076ca92079c9f18ebce2fb33ee17664654
Filipe <filiperotherds@gmail.com>
2025-08-03 21:35:15 -0300

    Prettier & ESLint

---
+++ config/eslint-config/library.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter} */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/react'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error'
+  }
+}
